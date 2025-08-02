4b7146f4ef27fb84dde43cbe1da2ceb03028fa94
MuhammedGazi <muhammed.celenk1@ogr.sakarya.edu.tr>
2025-08-02 16:38:40 +0300

    CQRS

---
+++ DesignPattern.CQRS/CQRSPattern/Commands/CreateProductCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPattern.CQRS.CQRSPattern.Commands
+{
+    public class CreateProductCommand
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ DesignPattern.CQRS/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.CQRS.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-GLCB4AC;initial catalog=DesignPattern2;integrated security=true;Encrypt=False;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
